72bca30991bffc3aa42501bc14a07461de10b31a
PetaBread <petersusanto545@gmail.com>
2024-05-28 22:21:50 -0400

    test

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+
+
+function ListGroup() {
+    return <h1>List Group</h1>;
+}
+
+export default ListGroup;
---
+++ src/message.tsx
@@ -0,0 +1,12 @@
+function Message() {
+    const name ="Peter"
+    return <h1>Hello {name}</h1>;
+}
+
+
+function Button() {
+    return <button>Click me!</button>
+}
+
+
+export default Message; 
